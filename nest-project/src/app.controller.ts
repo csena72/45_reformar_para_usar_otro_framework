c6bd04e2f9ff8c97ac283bcdb262b2ac5c233943
Sena, Cristian <cristian.sena.ext@boehringer-ingelheim.com>
2021-12-11 13:26:58 -0300

    Agrego eps

--- nest-project/src/app.controller.ts
+++ nest-project/src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProductDto } from './dto/products.dto';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPing(): string {
+    return this.appService.ping();
+  }
+
+  @Get('products')
+  getProducts(): string {
+    return this.appService.getProducts();
+  }
+
+  @Get('product/id')
+  getProduct(id: any): string {
+    return this.appService.getProduct(id);
+  }
+
+  @Post('product')
+  createProduct(@Body() product: ProductDto): any {
+    console.log(product);
+    return product;
+    //return this.appService.createProduct();
+  }
+
+  @Patch('product')
+  updateProduct(): string {
+    return this.appService.updateProduct();
+  }
+
+  @Delete('product')
+  deleteProduct(): string {
+    return this.appService.deleteProduct();
   }
 }
--- nest-project/src/app.service.ts
+++ nest-project/src/app.service.ts
@@ -2,7 +2,27 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPing(): string {
+    return 'Api Productos ok!';
+  }
+
+  getProducts(): string {
+    return 'productos';
+  }
+
+  getProduct(id: any): string {
+    return `Producto id: ${id}`;
+  }
+
+  createProduct(): string {
+    return 'producto creado';
+  }
+
+  updateProduct(): string {
+    return 'producto actualizado';
+  }
+
+  deleteProduct(): string {
+    return 'producto eliminado';
   }
 }
